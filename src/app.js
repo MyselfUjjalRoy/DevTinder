9eb2dd0d7c1a8175eb903111a456724fc2753a8f
Ujjal Roy <royu99099@gmail.com>
2025-08-26 19:11:31 +0530

    explored get post delete method

--- src/app.js
+++ src/app.js
@@ -2,13 +2,30 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello" ,(req , res)=>{
-  res.send("Hello from the server...hello");
+app.use("/user", (req, res) => {
+  res.send("Ha ha ha ha");
 });
 
+//this will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({firstname: "Ujjal" , lastname: "Royy"});
+});
+
+app.post("/user", (req, res) => {
+  res.send("Save Data to the database successfullyy");
+
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully");
+});
+
+//this will match all the HTTP method API calls to /test
 app.use("/test" ,(req , res)=>{
   res.send("Hello from the server...test");
 });
+
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
