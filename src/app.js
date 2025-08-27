45de0799405993a544ff94064b5c8b87b3a01e1c
Ujjal Roy <royu99099@gmail.com>
2025-08-27 15:34:09 +0530

    handled multiple routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,36 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user",(req, res , next)=>{
+  //route handler
+  console.log("Handling the route user 2!!")
+  next();
+  //res.send("Response 1");
+},
+(req, res , next)=>{
+  //route handler
+  console.log("Handling the route user 2!!")
+  next();
+  //res.send("Second Response");
+},
+(req, res , next)=>{
+  //route handler
+  console.log("Handling the route user 3!!")
+   next();
+   //res.send("Third Response");
+},
+(req, res , next)=>{
+  //route handler
+  console.log("Handling the route user 4!!")
+  //res.send("Fourth Response");
+  next();
+},
+(req, res , next)=>{
+  //route handler
+  console.log("Handling the route user 5!!")
+  res.send("Fifth Response");
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({firstname: "Ujjal" , lastname: "Roy"});
-});
-
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
